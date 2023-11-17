98f151ca53e64bf7c6e31b6f23e130183a7b8ffc
Marko <marko@wolfwarelabs.com>
2023-11-17 19:20:53 +0100

    Added template

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { writeFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import { getTRPCStructure, trpcStructureToSwiftClass } from "./generators/router.js";
 import { indentSwiftCode } from "./utility.js";
 import { SwiftTRPCRouterDef } from "./types.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export { extendZodWithSwift } from "./types.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const trpcRouterToSwiftClient = (name: string, routerDef: SwiftTRPCRouterDef): string => {
     const trpcStructure = getTRPCStructure(routerDef);
     const swiftClass = trpcStructureToSwiftClass(name, trpcStructure, { isRoot: true, depth: 0 });
 
-    let swiftClient = "import Foundation \n\n";
+    let swiftClient = readFileSync(path.join(__dirname, "templates/TRPCClient.swift")).toString("utf-8");
     swiftClient += swiftClass;
     return indentSwiftCode(swiftClient);
 };
