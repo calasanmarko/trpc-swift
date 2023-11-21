70cb1488214806936fdd3b5e02c3a3803ccfb268
Marko <marko@wolfwarelabs.com>
2023-11-21 17:35:27 +0100

    Swift Error Handling

--- templates/TRPCClient.swift
+++ templates/TRPCClient.swift
@@ -6,6 +6,64 @@
 
 import Foundation
 
+enum DecodableValue: Decodable {
+    case string(String)
+    case int(Int)
+    case double(Double)
+    case bool(Bool)
+    case dictionary([String: DecodableValue])
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let str = try? container.decode(String.self) {
+            self = .string(str)
+        } else if let int = try? container.decode(Int.self) {
+            self = .int(int)
+        } else if let dbl = try? container.decode(Double.self) {
+            self = .double(dbl)
+        } else if let bool = try? container.decode(Bool.self) {
+            self = .bool(bool)
+        } else {
+            let dict = try container.decode([String: DecodableValue].self)
+            self = .dictionary(dict)
+        }
+    }
+}
+
+enum TRPCErrorCode: Int, Codable {
+    // tRPC Defined
+    case parseError = -32700
+    case badRequest = -32600
+    case internalServerError = -32603
+    case unauthorized = -32001
+    case forbidden = -32003
+    case notFound = -32004
+    case methodNotSupported = -32005
+    case timeout = -32008
+    case conflict = -32009
+    case preconditionFailed = -32012
+    case payloadTooLarge = -32013
+    case unprocessableContent = -32022
+    case tooManyRequests = -32029
+    case clientClosedRequest = -32099
+    
+    // Application Defined
+    case unknown = -1
+    case missingOutputPayload = -2
+}
+
+struct TRPCError: Error, Decodable {
+    let code: TRPCErrorCode
+    let message: String?
+    let data: DecodableValue?
+
+    init(code: TRPCErrorCode, message: String? = nil, data: DecodableValue? = nil) {
+        self.code = code
+        self.message = message
+        self.data = data
+    }
+}
+
 struct TRPCRequest<T: Encodable>: Encodable {
     struct DataContainer: Encodable {
         let json: T?
@@ -27,7 +85,12 @@ struct TRPCResponse<T: Decodable>: Decodable {
         let data: DataContainer
     }
     
-    let result: Result
+    struct ErrorContainer: Decodable {
+        let json: TRPCError
+    }
+    
+    let result: Result?
+    let error: ErrorContainer?
 }
 
 typealias TRPCMiddleware = (URLRequest) async throws -> URLRequest
@@ -59,9 +122,7 @@ class TRPCClient {
             throw NSError(domain: "", code: -1, userInfo: nil)
         }
         
-        let response = try await send(url: url, httpMethod: "GET", middlewares: middlewares, bodyData: nil)
-        
-        return try JSONDecoder(dateDecodingStrategy: .formatted(dateFormatter)).decode([TRPCResponse<Response>].self, from: response)[0].result.data.json
+        return try await send(url: url, httpMethod: "GET", middlewares: middlewares, bodyData: nil)
     }
     
     func sendMutation<Request: Encodable, Response: Decodable>(url: URL, middlewares: [TRPCMiddleware], input: Request) async throws -> Response {
@@ -76,12 +137,10 @@ class TRPCClient {
             throw NSError(domain: "", code: -1, userInfo: nil)
         }
         
-        let response = try await send(url: url, httpMethod: "POST", middlewares: middlewares, bodyData: data)
-        
-        return try JSONDecoder(dateDecodingStrategy: .formatted(dateFormatter)).decode([TRPCResponse<Response>].self, from: response)[0].result.data.json
+        return try await send(url: url, httpMethod: "POST", middlewares: middlewares, bodyData: data)
     }
     
-    private func send(url: URL, httpMethod: String, middlewares: [TRPCMiddleware], bodyData: Data?) async throws -> Data {
+    private func send<Response: Decodable>(url: URL, httpMethod: String, middlewares: [TRPCMiddleware], bodyData: Data?) async throws -> Response {
         var request = URLRequest(url: url)
         request.httpMethod = httpMethod
         request.httpBody = bodyData
@@ -95,7 +154,21 @@ class TRPCClient {
         request.httpBody = bodyData
         
         let response = try await URLSession.shared.data(for: request)
-        return response.0
+        let decoded = try JSONDecoder(dateDecodingStrategy: .formatted(dateFormatter)).decode([TRPCResponse<Response>].self, from: response.0)[0]
+        
+        if let error = decoded.error {
+            throw error.json
+        }
+        
+        if let result = decoded.result {
+            return result.data.json
+        }
+        
+        if Response.self == EmptyObject.self {
+            return EmptyObject() as! Response
+        }
+        
+        throw TRPCError(code: .missingOutputPayload, message: "Missing output payload.", data: nil)
     }
 }
 
